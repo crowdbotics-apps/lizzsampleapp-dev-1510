0d4e95dfecdde82ebe10316b026efcc5ed074739
Crowdbotics <team@crowdbotics.com>
2020-01-24 00:48:17 +0000

    Update models

--- home/migrations/0003_car_engine.py
+++ home/migrations/0003_car_engine.py
@@ -9,31 +9,81 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Engine',
+            name="Engine",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('fuel', models.CharField(max_length=256)),
-                ('bhp', models.BigIntegerField()),
-                ('torque', models.FloatField()),
-                ('description', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='engine_description', to='home.CustomText')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("fuel", models.CharField(max_length=256)),
+                ("bhp", models.BigIntegerField()),
+                ("torque", models.FloatField()),
+                (
+                    "description",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="engine_description",
+                        to="home.CustomText",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Car',
+            name="Car",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('model', models.CharField(max_length=256)),
-                ('year', models.DateField()),
-                ('brand', models.CharField(max_length=256)),
-                ('doors', models.BigIntegerField()),
-                ('description', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='car_description', to='home.CustomText')),
-                ('engine', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='car_engine', to='home.Engine')),
-                ('owner', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='car_owner', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("model", models.CharField(max_length=256)),
+                ("year", models.DateField()),
+                ("brand", models.CharField(max_length=256)),
+                ("doors", models.BigIntegerField()),
+                (
+                    "description",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="car_description",
+                        to="home.CustomText",
+                    ),
+                ),
+                (
+                    "engine",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="car_engine",
+                        to="home.Engine",
+                    ),
+                ),
+                (
+                    "owner",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="car_owner",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
--- home/models.py
+++ home/models.py
@@ -8,6 +8,7 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    lizzcustomfield = models.FloatField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
@@ -44,16 +45,16 @@ class Car(models.Model):
     doors = models.BigIntegerField()
     engine = models.ForeignKey(
         "home.Engine",
-        on_delete=models.CASCADE,
         null=True,
         blank=True,
+        on_delete=models.CASCADE,
         related_name="car_engine",
     )
     description = models.ForeignKey(
         "home.CustomText",
-        on_delete=models.CASCADE,
         null=True,
         blank=True,
+        on_delete=models.CASCADE,
         related_name="car_description",
     )
 
@@ -65,8 +66,8 @@ class Engine(models.Model):
     torque = models.FloatField()
     description = models.ForeignKey(
         "home.CustomText",
-        on_delete=models.CASCADE,
         null=True,
         blank=True,
+        on_delete=models.CASCADE,
         related_name="engine_description",
     )
